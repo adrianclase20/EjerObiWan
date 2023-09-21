506ed5a020722a1f9f0873651ae663f7476f8ec5
adrianclase20 <adrianclase20@gmail.com>
2023-09-21 08:23:21 +0200

    ADD ObiWan.java
    añade el archivo ObiWan bien hecho

--- src/paqueteObiWan/ObiWan.java
+++ src/paqueteObiWan/ObiWan.java
@@ -1,77 +1,80 @@
 package paqueteObiWan;
-// MicroHelpApp.java
-
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ObiWan extends Application {
-    private Text textodeayuda = new Text();
 
     public static void main(String[] args) {
-        Application.launch(args);
+        // Lanza la aplicación JavaFX
+        launch(args);
     }
 
     @Override
     public void start(Stage escenario) {
+        // Configura el título de la ventana
+        escenario.setTitle("Verificación de Nombre");
+
+        // Crea un campo de texto para ingresar el nombre
         TextField nombre = new TextField();
-        TextField apellido = new TextField();
-        TextField salario = new TextField();
-        Button BtnCerrar = new Button("Close");
-        BtnCerrar.setOnAction(e -> Platform.exit());
-        nombre.getProperties().put("microHelpText", "Introduce tu nombre");
-        apellido.getProperties().put("microHelpText", "Introduce tu apellido");
-        salario.getProperties().put("microHelpText", "Ingrese un salario mayor a $2000.00");
-        // The help text node is unmanaged
-        textodeayuda.setManaged(false);
-        textodeayuda.setTextOrigin(VPos.TOP);
-        textodeayuda.setFill(Color.RED);
-        textodeayuda.setFont(Font.font(null, 9));
-        textodeayuda.setMouseTransparent(true);
-        // Add all nodes to a GridPane
-        GridPane root = new GridPane();
-        root.add(new Label("Nombre:"), 1, 1);
-        root.add(nombre, 2, 1);
-        root.add(new Label("Apellido:"), 1, 2);
-        root.add(apellido, 2, 2);
-        root.add(new Label("Salario:"), 1, 3);
-        root.add(salario, 2, 3);
-        root.add(BtnCerrar, 3, 3);
-        root.add(textodeayuda, 4, 3);
-        Scene escena = new Scene(root, 300, 100);
-        // Add a change listener to the scene, so we know when
-        // the focus owner changes and display the micro help
-        escena.focusOwnerProperty().addListener((ObservableValue<? extends Node> value, Node oldNode, Node newNode) -> focusChanged(value, oldNode, newNode));
+
+        // Crea un botón para verificar el nombre
+        Button btnVerificacion = new Button("Verificar");
+
+        // Crea una etiqueta para mostrar el resultado
+        Label greetingLabel = new Label();
+
+        // Configura una acción cuando se hace clic en el botón "Verificar"
+        btnVerificacion.setOnAction(e -> {
+            // Obtiene el nombre ingresado y elimina espacios en blanco al inicio y al final
+            String nombreIntroducido = nombre.getText().trim();
+
+            // Comprueba si el nombre es igual a "Obi wan" (sin importar mayúsculas o minúsculas)
+            if (nombreIntroducido.equalsIgnoreCase("Obi wan")) {
+                // Muestra una alerta con el mensaje "¡General Kenobi!"
+                mostrarAlerta("¡General Kenobi!");
+            } else {
+                // Muestra un saludo personalizado en la etiqueta de resultado
+                greetingLabel.setText("Hola " + nombreIntroducido);
+            }
+        });
+
+        // Crea un botón para cerrar la aplicación
+        Button btnCerrar = new Button("Cerrar");
+
+        // Configura una acción para cerrar la ventana principal al hacer clic en el botón "Cerrar"
+        btnCerrar.setOnAction(e -> escenario.close());
+
+        // Crea un diseño vertical para organizar los elementos en la ventana
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
+
+        // Agrega los elementos al diseño vertical
+        vbox.getChildren().addAll(nombre, btnVerificacion, greetingLabel, btnCerrar);
+
+        // Crea una escena con el diseño vertical y un tamaño de 300x200
+        Scene escena = new Scene(vbox, 300, 200);
+
+        // Establece la escena en el escenario (ventana principal)
         escenario.setScene(escena);
-        escenario.setTitle("Showing Micro Help");
+
+        // Muestra la ventana principal
         escenario.show();
     }
 
-    public void focusChanged(ObservableValue<? extends Node> value, Node nodoViejo, Node nodoNuevo) {
-        // Focus has changed to a new node
-        String microHelpText = (String) nodoNuevo.getProperties().get("microHelpText");
-        if (microHelpText != null && microHelpText.trim().length() > 0) {
-            textodeayuda.setText(microHelpText);
-            textodeayuda.setVisible(true);
-            // Position the help text node
-            double x = nodoNuevo.getLayoutX() + nodoNuevo.getLayoutBounds().getMinX() - textodeayuda.getLayoutBounds().getMinX();
-            double y = nodoNuevo.getLayoutY() + nodoNuevo.getLayoutBounds().getMinY() + nodoNuevo.getLayoutBounds().getHeight() - textodeayuda.getLayoutBounds().getMinX();
-            textodeayuda.setLayoutX(x);
-            textodeayuda.setLayoutY(y);
-            textodeayuda.setWrappingWidth(nodoNuevo.getLayoutBounds().getWidth());
-        } else {
-            textodeayuda.setVisible(false);
-        }
+    // Método para mostrar una alerta con un mensaje
+    private void mostrarAlerta(String mensaje) {
+        Alert alerta = new Alert(AlertType.INFORMATION);
+        alerta.setTitle("Mensaje");
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
     }
 }
